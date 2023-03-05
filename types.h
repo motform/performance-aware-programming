b34ea9cb985d00678365f86a18be636c736f6e17
motform <love.lagerkvist@protonmail.com>
2023-03-05 12:13:27 +0100

    Add str (pointer to memory + count)

--- types.h
+++ types.h
@@ -1,7 +1,17 @@
-#include <stdint.h>
-#include <stddef.h>
-#include <limits.h>
+#pragma once
+
+#include <assert.h>
 #include <float.h>
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+
+#define function static inline
 
 typedef int8_t  int8;
 typedef int16_t int16;
@@ -17,7 +27,46 @@ typedef uint64_t uint64;
 typedef intptr_t intptr;
 typedef uintptr_t uintptr;
 
-typedef size_t memory_index;
+typedef size_t size;
 
 typedef float float32;
 typedef double float64;
+
+typedef unsigned char byte;
+
+typedef struct str {
+  size        count;
+  const byte* buffer;
+} str;
+
+function size_t count_c_string(const char* c_string) {
+  size_t count = 0;
+  while (*c_string++) count++;
+  return count;
+}
+
+function str str_from_c_string(const char* c_string) {
+  str result = {
+	.count = count_c_string(c_string),
+	.buffer = (const byte*)c_string,
+  };
+
+  return result;
+}
+
+function void read_file_to_str(char* path, str* result) {
+  int32 file_descriptor = open(path, O_RDONLY, 0);
+  assert(file_descriptor != -1);
+
+  struct stat file_status;
+  if (stat(path, &file_status) == -1) {
+	printf("Unable to get file status.");
+  }
+
+  byte* buffer = malloc(file_status.st_size);
+  read(file_descriptor, buffer, file_status.st_size);
+  close(file_descriptor);
+
+  result->buffer = buffer;
+  result->count = file_status.st_size;
+}
