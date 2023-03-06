ca64a290b8079ec4abf47c7e609a1821a29b0b63
motform <love.lagerkvist@protonmail.com>
2023-03-06 14:58:15 +0100

    Add support for simple displacement operations

--- src/decoder.c
+++ src/decoder.c
@@ -9,6 +9,11 @@ typedef enum Register {
 	/* W = 1 */  AX, CX, DX, BX, SP, BP, SI, DI,
 } Register;
 
+static char* address_register_equation[] = {
+	"bx + si", "bx + di", "bp + si", "bp + di", 
+	"si",      "di",      "bp",      "bx"
+};
+
 static char* register_name[] = {
 	/* W = 0 */  "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh",
 	/* W = 1 */  "ax", "cx", "dx", "bx", "sp", "bp", "si", "di",
@@ -32,34 +37,77 @@ Register decode_register(byte W, byte reg) {
 bool32 decode(str instruction_stream) {
 	Decoder decoder = {
 		.instruction_stream = instruction_stream,
-		.position           = (byte*)instruction_stream.buffer,
-		.end                = (byte*)instruction_stream.buffer + instruction_stream.count,
-	};
+			.position           = (byte*)instruction_stream.buffer,
+			.end                = (byte*)instruction_stream.buffer + instruction_stream.count,
+			};
 
-	printf("bits 16\n");
+	printf("bits 16\n\n");
 
-	for (byte high_byte = read_byte(&decoder); decoder.position < decoder.end; high_byte = read_byte(&decoder)) {
+	for (byte high_byte = read_byte(&decoder);
+		 decoder.position < decoder.end;
+		 high_byte = read_byte(&decoder)) {
 
 		// 6bit prefixes
 		byte op = high_byte >> 2;
 		if (op == MOV_register_or_memory_to_from_register) {
 			byte D = (high_byte >> 1) & 0x1;
 			byte W =  high_byte       & 0x1;
-			/* printf("high_byte: %o OP: %o W: %o D: %o\n", high_byte, op, W, D); */
 
 			byte low_byte = read_byte(&decoder);
-
 			byte MOD = (low_byte >> 6) & 0x3;
 			byte REG = (low_byte >> 3) & 0x7;
 			byte R_M = (low_byte >> 0) & 0x7;
-			assert(MOD == 0x3); // We only support MOD = 11, i.e. MEM
-			/* printf("low_byte: %o MOD: %o REG: %o R_M: %o\n", low_byte, MOD, REG, R_M); */
 
 			Register target = decode_register(W, D ? REG : R_M);
 			Register source = decode_register(W, D ? R_M : REG);
-			/* printf("target: %o source: %o\n", target, source); */
 
-			printf("MOV %s, %s\n", register_name[target], register_name[source]);
+			switch (MOD) {
+
+			case 0b00: {
+				printf("MOV %s, [%s]\n",
+					   register_name[target],
+					   address_register_equation[source & 0x7]); // We don't want the extra byte set by W.
+			} break;
+
+			case 0b01: {
+				byte displacement = read_byte(&decoder);
+				char* displacement_equation = "";
+				int allocated_string_buffer = 0;
+
+				if (displacement) {
+					allocated_string_buffer = asprintf(&displacement_equation, " + %i", displacement);
+					assert(allocated_string_buffer != -1);
+				}
+
+				printf("MOV %s, [%s%s]\n",
+					   register_name[target],
+					   address_register_equation[source & 0x7],
+					   displacement_equation);
+				if (allocated_string_buffer) free(displacement_equation);
+			} break;
+
+			case 0b10: {
+				byte displacement_low  = read_byte(&decoder);
+				byte displacement_high = read_byte(&decoder);
+				uint16 displacement = (displacement_high << 8) | displacement_low;
+				printf("MOV %s, [%s + %i]\n",
+					   register_name[target],
+					   address_register_equation[source & 0x7],
+					   displacement);
+			} break;
+
+			case 0b11: {
+				printf("MOV %s, %s\n",
+					   register_name[target],
+					   register_name[source]);
+			} break;
+
+			default: {
+				printf("Unknown value of MOD: %o", MOD);
+				assert(0);
+			} break;
+
+			}
 
 			continue;
 		}
